fcc82d9accc9471394fc1b6d5d10f4e759d4d5f1
david <maldonadopereira@gmail.com>
2022-06-29 21:55:35 -0300

    Configuração usuario e criação da app post
    close #15

--- core/models.py
+++ core/models.py
@@ -1,3 +1,10 @@
-# from django.db import models
+from django.db import models
 
-# Create your models here.
+
+class Base(models.Model):
+    criado = models.DateField('Criação', auto_now_add=True)
+    modificado = models.DateField('Atualização', auto_now=True)
+    ativo = models.BooleanField('Ativo', default=True)
+
+    class Meta:
+        abstract = True
