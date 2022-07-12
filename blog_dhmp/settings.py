ef99b6558292e79d0ea83ea3644610e9ad7a5285
David <maldonadopereira@gmail.com>
2022-07-11 21:10:08 -0300

    Configurar armazenamento na AWS S3
    close #23

--- blog_dhmp/settings.py
+++ blog_dhmp/settings.py
@@ -31,7 +31,7 @@ DEBUG = config('DEBUG', cast=bool)
 ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
 
 
-# Application definition
+# Application definitions
 
 INSTALLED_APPS = [
     'django.contrib.admin',
